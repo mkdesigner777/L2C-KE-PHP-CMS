a297e808d33dbf38dc5cf94fc9eed85e1ca089b5
Stephan Bönnemann <stephan@boennemann.me>
2018-12-12 21:10:06 +0100

    3 uloha oprava

--- public/index.php
+++ public/index.php
@@ -22,6 +22,15 @@ $pages = db_select("SELECT * FROM pages");
 
 <body>
 
+<div class="blog-masthead">
+	<div class="container">
+		<nav class="blog-nav">
+			<a class="blog-nav-item active" href="#">Home</a>
+
+		</nav>
+	</div>
+</div>
+
 <div class="container">
 
 	<div class="blog-header">
@@ -29,25 +38,24 @@ $pages = db_select("SELECT * FROM pages");
 		<p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
 	</div>
 
-
-
 	<div class="row">
 
 		<div class="col-sm-8 blog-main">
 
 			<?php foreach($pages as $page){ ?>
-			<h2 class= "blog-post-title"><?php echo $page->title; ?></h2>
+				<?php $user= db_select("SELECT * FROM users WHERE ID =".$page->User_ID);?>
 
-			<?php
-				$user = db_select("SELECT * FROM users where ID=".$page->User_ID)
-			?>
-			<p class="blog-post-meta">December 14, 2013 by <a href="#"><?php echo $user[0]->email; ?></a></p>
+				<div class="blog-post">
+					<h2 class="blog-post-title"><?php echo $page->title; ?></h2>
+					<p class="blog-post-meta">December 14, 2013 by <a href="#"><?php echo $user[0]->email; ?></a></p>
 
-			<p><?php echo $page->content; ?></p>
+					<p><?php echo $page->content; ?></p>
 
-		</div><!-- /.blog-post -->
+				</div><!-- /.blog-post -->
 
-<?php }?>
+			<?php } ?>
+
+		</div><!-- /.blog-main -->
 
 		<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
 			<div class="sidebar-module sidebar-module-inset">
@@ -57,32 +65,27 @@ $pages = db_select("SELECT * FROM pages");
 
 		</div><!-- /.blog-sidebar -->
 
+	</div><!-- /.row -->
 
-	</div><!-- /.container -->
+</div><!-- /.container -->
 
-	<footer class="blog-footer">
-		<p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-		<p>
-			<a href="#">Back to top</a>
-		</p>
-	</footer>
+<footer class="blog-footer">
+	<p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+	<p>
+		<a href="#">Back to top</a>
+	</p>
+</footer>
 
 
-	<!-- Bootstrap core JavaScript
-	================================================== -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-	<script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-</body></html>
+</body>
+</html>
 
-<div class="blog-masthead">
-	<div class="container">
-		<nav class="blog-nav">
-			<a class="blog-nav-item active" href="#">Home</a>
-		</nav>
-	</div>
-</div>
 
 
 
